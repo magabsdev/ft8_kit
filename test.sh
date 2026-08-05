89da8cfdde65ef943e7bce8fe6a06c3639c68803
Mark Gregory <mark.gregory@mpba.co.uk>
2026-08-05 09:02:36 +0100

    Diagsnostic testing

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
-#swift package clean
-#swift build --product ft8-validate
-#swift test
-#.build/debug/ft8-validate decode --diagnostics --dump-debug /tmp/ft8-trace-checkpoint-2 ft8_lib/test/wav/191111_110130.wav
-
 rm -rf .build
 swift package clean
 swift build --product ft8-validate
