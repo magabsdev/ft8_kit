267a187c0379fd469ee141b6ac2403782b3bc988
Mark Gregory <mark.gregory@mpba.co.uk>
2026-08-08 12:33:58 +0100

    latest updates to improve decode
    Signed-off-by: Mark Gregory <mark.gregory@mpba.co.uk>

--- Tools/testit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd .. && clear && swift build --product ft8-validate && perl -e '
-    alarm 60;
-    exec @ARGV
-' .build/debug/ft8-validate \
-  decode \
-  --diagnostics \
-  --json \
-  ft8_lib/test/wav/191111_110130.wav
